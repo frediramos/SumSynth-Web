e073bd802266d88d44be609c68ac610d9a1e6a5a
Frederico Ramos <frediramos@gmail.com>
2024-01-19 22:15:55 +0000

    Add first implementation of server endpoint

---
+++ server/index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+
+// Dont need to block CPU to gen a temp string
+// Can use 'non-secure'
+const nanoid = require('nanoid/non-secure')
+
+app.use(express.json())
+
+const languages = ['c', 'py']
+const generators = ['under', 'over', 'exact']
+
+const postSchema = Joi.object({
+    spec: Joi.string().required(),
+    gen: Joi.string().valid(...generators).required(),
+    lang: Joi.string().valid(...languages).required(),
+});
+
+
+function tmpName(){
+    let name = nanoid.nanoid();
+    while(fs.existsSync(name)){
+        name = nanoid.nanoid();
+    }
+    return name;
+}
+
+function deleteFile(file){
+    if(fs.existsSync(file)){
+        fs.unlinkSync(file);
+    }
+}
+
+// spec: handwritten input spec
+// gen: under|over|exact generator
+// lang: c|py| output language
+
+function genSummary(spec, gen, lang){
+    const test_file = "test_files/strlen.json"
+    let summ = "";
+
+    const tmp_spec = tmpName();
+    // const tmp_json = tmpName();
+    const tmp_json = test_file;
+    const tmp_summ = tmpName();
+
+    try{
+        // fs.writeFileSync(tmp_spec, spec)
+        
+        // let 'spec2json' be a symlink to a sumsynth binary
+        //  (compiled haskell with stack build?)
+        
+        // execSync(`spec2json ${spec} ${tmp_json} ${gen}`);   
+        
+        execSync(`json2summ -${lang} ${tmp_json} -o ${tmp_summ}`);   
+        summ = fs.readFileSync(tmp_summ).toString();
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    deleteFile(tmp_spec);
+    // deleteFile(tmp_json);
+    deleteFile(tmp_summ);
+
+    return summ;
+}
+
+app.post('/gen', (req, res) => {
+
+    const result = postSchema.validate(req.body);
+    
+    if (result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    
+    const spec = req.body.spec;
+    const gen = req.body.gen;
+    const lang = req.body.lang;
+    
+    console.log(`Request Received:\n spec: ${spec}\n generator: ${gen}\n language: ${lang}\n`)
+    
+    const summ = genSummary(spec, gen, lang)
+
+    const summary = {   
+        lang: lang,
+        code: summ
+    };
+    
+    res.send(summary);
+});    
+
+
+const port = 3000
+app.listen(port, () => console.log(`Listening on port ${port}\n`));
